5acb0a7d9f07de4ff8af78f82cc0319cd9acd76e
john mauran <johnmauran@johns-MacBook-Air.local>
2017-04-19 16:56:19 -0400

    Release feature round 2

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,7 +1,6 @@
 class Release < ApplicationRecord
   validates :title, presence: true
   validates :artist, presence: true
-  validates :performer
   validates :year, presence: true, numericality: true
   validates :studio, presence: true
   validates :no_of_tracks, presence: true, numericality: true
