77757d1d3bf7534e8fac9a9eb4ed259509cd9533
Donathan Raymond <donathan.raymond@gmail.com>
2017-04-19 11:17:59 -0400

    updated user model tests and feature tests

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -7,6 +7,29 @@ feature 'user signs up' do
   scenario 'user provides valid information in the sign-up form' do
     visit root_path
     expect(page).to have_content "Sign Up"
+
+    click_link "Sign Up"
+    fill_in "Username", with: 'flapjack1'
+    fill_in "Email", with: 'name@email.com'
+    fill_in "user_password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Sign up"
+
+    expect(page).to have_content "Welcome! You have signed up successfully."
+    expect(page).to have_content "Sign Out"
+    expect(page).to_not have_content "Sign Up"
+  end
+
+  scenario 'user provides no information in sign-up form' do
+    visit root_path
+
+    click_link "Sign Up"
+    click_button "Sign up"
+
+    expect(page).to have_content "Username can't be blank"
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Password can't be blank"
+    expect(page).to have_content "Sign Up"
   end
 end
 #I should see a link on the page to sign-up
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:username).when('testname', 'test_name') }
+  it { should_not have_valid(:username).when('my..name', 123) }
 end
