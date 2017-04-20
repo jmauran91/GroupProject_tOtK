872db20c0811c476219acee7584bcb0e7d6cb2c9
john mauran <johnmauran@johns-MacBook-Air.local>
2017-04-20 16:41:23 -0400

    Integrate changes from pull?

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,10 +1,11 @@
 class Release < ApplicationRecord
   validates :title, presence: true
   validates :artist, presence: true
-  validates :year, presence: true, inclusion: { in: 1850..Date.today.year }
+  validates :year, presence: true, numericality: { only_integer: true }
   validates :studio, presence: true
   validates :no_of_tracks, presence: true, numericality: true
   validates :album_art_url, format: { with: /\A(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/, message: "Invalid URL" }
 
   belongs_to :user
+  has_many :reviews
 end
---
+++ app/models/review.rb
@@ -0,0 +1,11 @@
+class Review < ApplicationRecord
+
+  validates :body, presence: true
+  validates :rating, numericality: true
+  validates :votes, numericality: true
+
+  belongs_to :user
+  belongs_to :release
+  has_many :comments
+
+end
