763634e2b78b7fc2fffab144afb66b545dc6dd61
Donathan Raymond <donathan.raymond@gmail.com>
2017-04-20 15:57:18 -0400

    Added error messages to the release form

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,10 +1,10 @@
 class Release < ApplicationRecord
   validates :title, presence: true
   validates :artist, presence: true
-  validates :year, presence: true, inclusion: { in: 1850..Date.today.year }
+  validates :year, presence: true, inclusion: { in: 1850..Date.today.year, message: 'is not valid' }
   validates :studio, presence: true
   validates :no_of_tracks, presence: true, numericality: true
-  validates :album_art_url, format: { with: /\A(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/, message: "Invalid URL" }
+  validates :album_art_url, format: { with: /\A(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/, message: "is invalid" }
 
   belongs_to :user
 end
