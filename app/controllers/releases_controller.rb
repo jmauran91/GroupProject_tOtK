8ca15b38f2f6b066e5a2e3d28a641f8761c17493
john mauran <johnmauran@johns-MacBook-Air.local>
2017-04-20 13:58:06 -0400

    Hard reset

---
+++ app/controllers/releases_controller.rb
@@ -0,0 +1,41 @@
+class ReleasesController < ApplicationController
+
+  def index
+    @releases = Release.all
+  end
+
+  def show
+    @release = Release.find(params[:id])
+  end
+
+  def new
+    @release = Release.new
+  end
+
+  def create
+    if current_user
+      @user = current_user
+      @release = Release.new(release_params)
+      @release.user = @user
+      @release.save
+      redirect_to root_path
+    else
+      flash[:notice] = "You must be logged in"
+      redirect_to new_user_session_path
+    end
+  end
+
+  private
+
+  def release_params
+    params.require(:release).permit(
+      :title,
+      :artist,
+      :performer,
+      :year,
+      :studio,
+      :no_of_tracks,
+      :album_art_url
+    )
+  end
+end
