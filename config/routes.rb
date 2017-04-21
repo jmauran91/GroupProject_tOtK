0bd4ae70844a5c95c520d16a5c56e4ff248c3a4e
john mauran <johnmauran@johns-MacBook-Air.local>
2017-04-21 15:01:37 -0400

    Add update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :releases, only: [:index, :show, :create, :new, :destroy] do
-    resources :reviews, only: [:show, :create, :new, :destroy, :patch]
+    resources :reviews, only: [:show, :create, :new, :destroy, :patch, :update]
   end
   resources :users, only: [:show]
 
