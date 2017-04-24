e98e359286d37723e54414494ecdd521045a5727
Donathan Raymond <donathan.raymond@gmail.com>
2017-04-24 13:42:51 -0400

    wrote test for user pulling reviews by genre

---
+++ spec/features/genres/user_can_view_releases_by_genre_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'The user can view releases by genre' do
+  scenario 'picks a genre from the top bar and selects a specific release to show' do
+    User.create!(
+      username: 'jbone91',
+      email: 'johnmauran1@gmail.com',
+      password: 'noneofyourbusiness'
+      )
+    visit new_user_session_path
+    fill_in 'Email', with: 'johnmauran1@gmail.com'
+    fill_in 'Password', with: 'noneofyourbusiness'
+    click_button 'Log in'
+
+    click_link "Add Release"
+    expect(page).to have_content "New Release Form"
+
+    fill_in 'Title', with: 'Great Album'
+    fill_in 'Artist', with: 'Great Band'
+    fill_in 'Performer', with: 'Great Hands'
+    fill_in 'Year', with: 1979
+    fill_in 'Studio', with: 'Great Studio'
+    fill_in 'No of tracks', with: 9
+    fill_in 'Album art url', with: 'http://tasteofcountry.com/files/2011/03/ralph-mooney.jpg'
+    page.check 'Blues'
+    page.check 'Jazz'
+
+    click_button 'Add Release'
+
+    find('a[href$="/genres/2"]').click #Blues Genre
+
+    expect(page).to have_content "Great Album"
+
+    find('a[href$="/genres/12"]').click #Jazz Genre
+
+    expect(page).to have_content "Great Album"
+  end
+end
