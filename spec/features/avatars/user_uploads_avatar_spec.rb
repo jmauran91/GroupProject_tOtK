8539c2dcae87627f71a3bc651bd7d34dcf119910
Donathan Raymond <donathan.raymond@gmail.com>
2017-04-26 13:18:49 -0400

    wrote passing tests for a user being able to upload and remove an avatar

--- spec/features/avatars/user_uploads_avatar_spec.rb
+++ spec/features/avatars/user_uploads_avatar_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 feature 'user can upload an avatar to their profile' do
   let!(:user) { FactoryGirl.create(:user) }
-  scenario 'user uploads avatar' do
+
+  before :each do
     visit root_path
 
     click_link "Sign In"
@@ -10,11 +11,31 @@ feature 'user can upload an avatar to their profile' do
     fill_in "Password", with: 'password'
     click_button "Log in"
 
-    expect(page).to have_content 'Sign Out'
-    expect(page).to_not have_content 'Sign In'
+    click_link "My Profile"
+    click_link "Edit my profile"
+
+    attach_file :user_avatar, "#{Rails.root}/spec/support/images/bass_guitar_hero_by_karate_chop.jpg"
+    fill_in "Current password", with: 'password'
+
+    click_button "Update"
+
+    click_link "My Profile"
   end
 
-  xscenario 'user removes avatar' do
+    scenario 'user uploads avatar' do
 
+    expect(page).to have_css("img[src*='bass_guitar_hero_by_karate_chop.jpg']")
+
+  end
+
+  scenario 'user removes avatar' do
+
+    click_link "Edit my profile"
+    check "Remove avatar"
+    fill_in "Current password", with: 'password'
+    click_button "Update"
+
+    click_link "My Profile"
+    expect(page).to_not have_css("img[src*='bass_guitar_hero_by_karate_chop.jpg']")
   end
 end
