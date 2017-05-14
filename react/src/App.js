37c54df52103bb3e3ad2486eb344bb172f9590a3
Donathan Raymond <donathan.raymond@gmail.com>
2017-05-14 16:56:03 -0400

    updated App.js

--- react/src/App.js
+++ react/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import {ReactDOM, render} from 'react-dom';
 import { Route, IndexRoute, Router, browserHistory } from 'react-router';
 import ReleaseIndexContainer from './containers/ReleaseIndexContainer';
+import ReviewContainer from './containers/ReviewContainer'
 
 const App = (props) => {
   return(
     <Router history={browserHistory}>
     <Route path="/" component={ReleaseIndexContainer} />
+    <Route path="/releases/:id" component={ReviewContainer} />
   </Router>
 
   )
